5f12b06504bc5132e63deead294dce1b1d6cf14a
anselbrandt <ansel@anselbrandt.com>
2020-06-29 07:20:31 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => <div>Welcome to React with Webpack 5!</div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
